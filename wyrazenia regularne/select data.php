eb78ee93ef52dee1051b35a14e629852cc6863ca
turboKRYSTIjelly13pl <ksuski8585@gmail.com>
2025-11-03 15:18:01 +0100

    03.11.2025

--- wyrazenia regularne/select data.php
+++ wyrazenia regularne/select data.php
@@ -1,89 +1,75 @@
 <?php
-$servername = "localhost";
-$username = "root"; //root
-$password = ""; // ""
-$database="5tia";
-//Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-//Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
+    $conn = new mysqli("localhost","root","","sklep");
+?>
 
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sklep dla uczniów</title>
+        <link rel="stylesheet" href="styl.css">
+    </head>
+    <body>
+        <header>
+            <h1>Dzisiejsze promocje naszego sklepu</h1>
+        </header>
 
+        <div id="lewy">
+            <h2>Taniej o 30%</h2>
+            <ol>
+                <?php
+                    $sql = "SELECT nazwa FROM towary WHERE promocja = 1;";
+                    $result = $conn->query($sql);
 
+                    while($row = $result -> fetch_array()) {
+                        echo "<li>$row[0]</li>";
+                    }
+                ?>
+            </ol>
+        </div>
 
+        <div id="srodkowy">
+            <h2>Sprawdź cenę</h2>
+            <form action="index.php" method="post">
+                <select name="towar" id="towar">
+                    <option value="Gumka do mazania">Gumka do mazania</option>
+                    <option value="Cienkopis">Cienkopis</option>
+                    <option value="Pisaki 60 szt.">Pisaki 60 szt.</option>
+                    <option value="Markery 4 szt.">Markery 4 szt.</option>
+                </select>
+                <button type="submit" name="check" id="check">SPRAWDŹ</button>
+            </form>
+            <?php
+                if(isset($_POST["check"])) {
+                    echo "<div id='script'>";
+                        $towar = $_POST["towar"];
+                        $sql = "SELECT cena FROM towary WHERE nazwa = '$towar';";
+                        $result = $conn->query($sql);
 
+                        while($row = $result -> fetch_array()) {
+                            $cena = $row[0];
+                            $cenanowa = $cena - ($cena * 0.3);
+                            echo "cena regularna: $cena<br>";
+                            echo "cena w promocji 30%: $cenanowa";
+                        }
+                    echo "</div>";
+                }
+            ?>
+        </div>
 
+        <div id="prawy">
+            <h2>Kontakt</h2>
+            <p>e-mail: <a href="mail to:bok@sklep.pl">bok@sklep.pl</a></p>
+            <img src="promocja.xcf" alt="promocja">
+        </div>
 
+        <footer>
+            <h4>Autor strony: <p>Krystian Suski 5tia nr18</p></h4>
+        </footer>
+    </body>
+</html>
 
-
-
-
-
-
-
-
-
-//Create database
-/*$sql = "CREATE DATABASE $database";
-if( mysqli_query($conn,$sql)) {
-    echo "Database created successfully";
-} else {
-    echo "Error creating database: " . mysqli_error($conn);
-}*/
-if (mysqli_select_db($conn, $database)) {
-    echo "Database $database selected";
-} else {
-    echo "Error select database: " .
-    mysqli_error($conn);
-}
-/*$sql = "CREATE TABLE uczniowie (
-        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        imie VARCHAR(30) NOT NULL,
-        nazwisko VARCHAR(30) NOT NULL,
-        email VARCHAR(50),
-        reg_date Timestamp Default
-        CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-        )";
-if (mysqli_query($conn, $sql)) {
-    echo "Table uczniowie created succesfully";
-} else {
-    echo "Error creating table: " . mysqli_error($conn);
-VALUES ('Maciej','Kowalski','MKowal1337@gmail.com')";
-if (mysqli_query($conn, $sql)){
-    echo "New record created successfully";
-} else {
-   }
-$sql = "INSERT INTO uczniowie (imie, nazwisko, email)
- echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
-$result = mysqli_query($conn, $sql);
-
-
-
-
-if (mysqli_num_rows($result) > 0) {
-    //output data of each row
-    while($row = mysqli_fetch_row($resoult)) {
-        echo "id:" . $row[0]. " - Name: " .
-        $row[1]. " " . $row[2]. "<br>";
-    }
-} else {
-    echo "0 results";
-}*/
-$sql = "SELECT id, imie, nazwisko FROM Uczniowie";
-$result = mysqli_query($conn, $sql);
-if (mysqli_num_rows($result) > 0) {
-    echo "<table>";
-    while($row = mysqli_fetch_row($result)) {
-        echo "<tr><td>" . $row[0]. "</td><td>" .
-        $row[1]. "</td><td>" . $row[2]. "</td></td>";
-    }
-    echo "</table>";
-} else {
-    echo "0 results";
-}
-    ?>
+<?php
+    $conn -> close();
+?>
